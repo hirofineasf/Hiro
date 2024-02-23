a31561937a77880f6ae078fcb4f05b5fe7d439fd
hirofineasf <159620765+hirofineasf@users.noreply.github.com>
2024-02-23 08:43:54 +0000

    import os, sys, platform os.system('clear') try:os.system('rm -rf HIRO') except:pass os.system("pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests") try:os.system('git pull')

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os, sys, platform
 os.system('clear')
-try:os.system('rm -rf KAIDO')
+try:os.system('rm -rf Hiro')
 except:pass
 try:os.system('rm -rf RAND')
 except:pass
@@ -11,9 +11,9 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     print("\x1b[1;93mDOWNLOADING UPDATE...\x1b[1;92m\n")
-    os.system('curl -L https://github.com/KAIDO-143/config/blob/main/NILLXD?raw=true -o NILLXD')
+    os.system('curl -L https://github.com/hirofineasf/config/blob/main/NILLXD?raw=true -o NILLXD')
     os.system('unzip NILLXD > /dev/null')
     os.remove('NILLXD')
-    os.system('chmod 777 KAIDO && ./KAIDO')
+    os.system('chmod 777 HIRO && ./Hiro')
 elif bit == '32bit':
     exit("\x1b[1;91mSORRY BRO 32 BIT NOT SUPPORTED\x1b[1;97m")
